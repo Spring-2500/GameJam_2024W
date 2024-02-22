e770d3c713a96c72336f88c59e14c85da9b65982
spring <sharp13neo@gmail.com>
2024-02-22 16:11:24 +0900

    プレイヤー操作変更、フレームレート60固定

--- Assets/Morita/Scripts/PCon.cs
+++ Assets/Morita/Scripts/PCon.cs
@@ -7,8 +7,8 @@ using UnityEngine.SceneManagement;
 public class PCon : MonoBehaviour
 {
     [SerializeField] float speed = 0.4f; //プレイヤーの目に進む速度
-    [SerializeField] float rightSpeed = 0.13f; //右に移動する速度
-    [SerializeField] float leftSpeed = -0.13f; //左に移動する速度
+    [SerializeField] float rightSpeed = 0.18f; //右に移動する速度
+    [SerializeField] float leftSpeed = -0.18f; //左に移動する速度
     [SerializeField] float jumpPower = 6.5f; //ジャンプの高さ
     [SerializeField] float moveStopTime = 1; //オブジェクトに衝突したときプレイヤーが止まる秒数
     [SerializeField] float startTime = 3.0f; //スタート時のカウントダウン
@@ -25,14 +25,18 @@ public class PCon : MonoBehaviour
 
 
     //入力されるキーを取得する変数
-    Dictionary<string, bool> move = new Dictionary<string, bool>
+   /* Dictionary<string, bool> move = new Dictionary<string, bool>
     {
         {"right", false },
         {"left", false },
-    };
+    };*/
 
     private void Start()
     {
+        //フレームレート固定
+        QualitySettings.vSyncCount = 0;
+        Application.targetFrameRate = 60;
+
         rb = GetComponent<Rigidbody>(); //Rigidbodyを取得
         isJumping = true; //最初はジャンプできるようにする
 
@@ -44,9 +48,16 @@ public class PCon : MonoBehaviour
     private void Update()
     {
         //入力されたキーを取得
-        move["right"] = Input.GetKey(KeyCode.D);
-        move["left"] = Input.GetKey(KeyCode.A);
+        /* move["right"] = Input.GetKey(KeyCode.D);
+         move["left"] = Input.GetKey(KeyCode.A);*/
 
+        if (moveStart)
+        {
+            if (Input.GetKey(KeyCode.D)) transform.Translate(rightSpeed, 0, 0);
+
+            if (Input.GetKey(KeyCode.A)) transform.Translate(leftSpeed, 0, 0);
+        }
+        
         //スペースキーを取得しジャンプする。空中ジャンプはできない
         if (Input.GetKeyDown(KeyCode.Space) && isJumping) Jump();
     }
@@ -56,8 +67,8 @@ public class PCon : MonoBehaviour
         //入力されたキーに応じてプレイヤーが移動
         if (moveStart)
         {
-            if (move["right"]) transform.Translate(rightSpeed, 0, 0); //速度rightSpeedで右に移動
-            if (move["left"]) transform.Translate(leftSpeed, 0, 0); //速度leftSpeedで左に移動
+           /* if (move["right"]) transform.Translate(rightSpeed, 0, 0); //速度rightSpeedで右に移動
+            if (move["left"]) transform.Translate(leftSpeed, 0, 0); //速度leftSpeedで左に移動*/
 
             //プレイヤーが自動的に前に進む
             transform.position += Vector3.forward * speed;
